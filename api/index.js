3bb865bba33ad8d85110b0cfe4519e225770b6bd
SANTIAGO <santiagokazlauskas@gmail.com>
2023-06-12 15:15:40 -0300

    asd

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 
-const Server = require("./models/server");
+const Server = require("../models/server");
 
 const server = new Server();
 
